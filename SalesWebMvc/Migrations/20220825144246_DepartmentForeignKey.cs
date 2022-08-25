8f4a615dcf85414448ce53b2a80849b641e3b04b
Leo Campos <leopcampos85@gmail.com>
2022-08-25 14:37:59 -0300

    Modificações no CSS

---
+++ SalesWebMvc/Migrations/20220825144246_DepartmentForeignKey.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SELLER_DEPARTMENT_DepartmentId",
+                table: "SELLER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "SELLER",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SELLER_DEPARTMENT_DepartmentId",
+                table: "SELLER",
+                column: "DepartmentId",
+                principalTable: "DEPARTMENT",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SELLER_DEPARTMENT_DepartmentId",
+                table: "SELLER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "SELLER",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SELLER_DEPARTMENT_DepartmentId",
+                table: "SELLER",
+                column: "DepartmentId",
+                principalTable: "DEPARTMENT",
+                principalColumn: "ID");
+        }
+    }
+}
